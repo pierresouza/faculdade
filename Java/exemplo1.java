ea11925c8d3d6be5a7a3850eea1b4b43ddb1e73d
pierresouza <pierre.s3@hotmail.com>
2024-02-02 13:35:32 -0300

    feat: create new exercises from java classes

---
+++ Java/exemplo1.java
@@ -0,0 +1,5 @@
+public class exemplo1 {
+    public static void main(String[] args){
+        System.out.println("Pierre Souza");
+    }
+}
---
+++ Java/principal.java
@@ -0,0 +1,8 @@
+public class principal {
+    public static void main(String[] args){
+        System.out.println(9 + 2);
+        System.out.println(9 - 2);
+        System.out.println(9 * 2);
+        System.out.println(9 / 2);
+    }
+}
